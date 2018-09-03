43a974b2dd3ad5cc3753c3ae71b616b279144ca8
Alberto <Albertoimpl@gmail.com>
2018-09-03 20:45:38 +0200

    {Updating urls}

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ fi
 echo "Deploying public folder..." 
 
 echo "Generating theme with url" 
-hugo -b http://albertoimpl.com -t crisp
+hugo -b http://albertoimpl.com/ -t crisp
 
 echo "Copying public folder to the github.io one"
 cp -R public/  ../albertoimpl.github.io
