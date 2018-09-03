fdc1abeaa3663f0bd2a439194fd73b1013e5a04b
Alberto <Albertoimpl@gmail.com>
2018-09-03 20:35:55 +0200

    {Updating deployment script}

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 if [ "$#" != 1 ];
     then
     echo "Usage deploy.sh \"Commit message\""
@@ -12,8 +14,13 @@ hugo -b http://albertoimpl.com -t crisp
 echo "Copying public folder to the github.io one"
 cp -R public/  ../albertoimpl.github.io
 
-cd ../albertoimpl.github.io
+pushd ../albertoimpl.github.io
+    git add -A
+    echo "Committing with message \"$1\""
+    git commit -m "{$1}"
+    git push origin master
+popd
+
 git add -A
-echo "Committing with message \"$1\""
-git commit -m $1
+git commit -m "{$1}"
 git push origin master
