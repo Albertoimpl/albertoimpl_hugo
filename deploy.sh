04fc9bbc2a70b05fea3cc7dea83b4fb8c4ff921e
Alberto <Albertoimpl@gmail.com>
2018-09-03 20:46:47 +0200

    Updating deployment script

--- deploy.sh
+++ deploy.sh
@@ -17,10 +17,10 @@ cp -R public/  ../albertoimpl.github.io
 pushd ../albertoimpl.github.io
     git add -A
     echo "Committing with message \"$1\""
-    git commit -m "{$1}"
+    git commit -m "$1"
     git push origin master
 popd
 
 git add -A
-git commit -m "{$1}"
+git commit -m "$1"
 git push origin master
